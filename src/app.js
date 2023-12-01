2cd72c903a1e994740ad886c779dfcf7e6144ca9
Diego Rodriguez <diego25598@gmail.com>
2023-12-01 04:32:07 +0100

    import projects routes

--- src/app.js
+++ src/app.js
@@ -1,5 +1,20 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+
+// Import routes
+
+import projectRoutes from "../routes/projects.routes.js";
+//import taskRoutes from "../routes/tasks.routes.js";
+
+// Middlewares
+
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
+app.use("/projects", projectRoutes);
+//sapp.use("/api/tasks", taskRoutes);
 
 export default app;
