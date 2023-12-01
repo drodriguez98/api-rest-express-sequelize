3ccbd2d44cc4f8fb694d7e65bb8514af68776cdc
Diego Rodriguez <diego25598@gmail.com>
2023-12-01 04:35:20 +0100

    delete imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import app from './app.js'
 import { sequelize } from './database/database.js'
 
-import './models/Project.js'
-import './models/Task.js'
-
 async function main() {
 
     try {
@@ -11,7 +8,9 @@ async function main() {
         //await sequelize.authenticate()
         //console.log('Connection has been established successfully.')
 
-        await sequelize.sync({ force: false }) // crear tablas de los modelos si no existen (force: true para crearlas aunque ya existan)
+        // Crear tablas de los modelos si no existen (force: true para crearlas aunque ya existan)
+
+        await sequelize.sync({ force: false }) 
 
         app.listen(3000)
         console.log('Server listening on port 3000')
