1eb80e544c168912f02678daa9e6bfc76de302a7
Diego Rodriguez <diego25598@gmail.com>
2023-12-01 03:22:08 +0100

    disable force at tables creation

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import app from './app.js'
 import { sequelize } from './database/database.js'
 
+import './models/Project.js'
+import './models/Task.js'
+
 async function main() {
 
     try {
 
-        await sequelize.authenticate()
-        console.log('Connection has been established successfully.')
+        //await sequelize.authenticate()
+        //console.log('Connection has been established successfully.')
+
+        await sequelize.sync({ force: false }) // crear tablas de los modelos si no existen (force: true para crearlas aunque ya existan)
 
         app.listen(3000)
         console.log('Server listening on port 3000')
